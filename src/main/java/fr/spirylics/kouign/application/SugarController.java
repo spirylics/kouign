b92350d14c42c796f8992dd499020631d6dd39e1
Cyril Souti <cyril.souti@deveryware.com>
2025-11-28 19:08:45 +0100

    fold

--- src/main/java/fr/spirylics/kouign/application/SugarController.java
+++ src/main/java/fr/spirylics/kouign/application/SugarController.java
@@ -3,15 +3,20 @@
  */
 package fr.spirylics.kouign.application;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Gatherers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/{version}/sugar")
 public class SugarController {
-    @GetMapping(path = "gatherer", version = "1.0")
-    public String gatherer() {
-        return "gatherer";
+    @GetMapping(path = "fold", version = "1.0")
+    public Map<String, ?> fold(@RequestParam final Collection<Integer> numbers) {
+        final var fold = numbers.stream().gather(Gatherers.fold(() -> 0, Integer::sum));
+        return Map.of("list", fold.toList());
     }
 }
